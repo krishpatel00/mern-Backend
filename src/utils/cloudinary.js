6cb4d4a4919cdf96d24d2d2921c74e017186241f
Krish Patel <krishpatel9484k@gmail.com>
2024-04-02 16:01:47 +0530

    full setup for backend

---
+++ src/utils/cloudinary.js
@@ -0,0 +1,25 @@
+import { v2 as cloudinary } from "cloudinary";
+import fs from "fs";
+
+cloudinary.config({
+  cloud_name: process.env.CLOUDINARY_CLOUD_NAME,
+  api_key: process.env.CLOUDINARY_API_NAME,
+  api_secret: process.envCLOUDINARY_API_SECERET,
+});
+
+const uploadOnCloudinary = async (localFilePath) => {
+  try {
+    if (!localFilePath) return null;
+    // upload file successfully
+    const response = await cloudinary.uploader.upload(localFilePath, {
+      resource_type: "auto",
+    });
+    console.log("upload file successfully", response.url);
+    return response;
+  } catch (error) {
+    fs.unlinkSync(localFilePath);
+    return null;
+  }
+};
+
+export {uploadOnCloudinary}
